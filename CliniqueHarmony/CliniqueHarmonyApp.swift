9d1579006a59975c3be138a4a306402a07a538bc
Ana-Bogdan <ana2004bogdan@gmail.com>
2025-11-09 21:10:51 +0200

    Initial Commit

---
+++ CliniqueHarmony/CliniqueHarmonyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CliniqueHarmonyApp.swift
+//  CliniqueHarmony
+//
+//  Created by Ana Bogdan on 09.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct CliniqueHarmonyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CliniqueHarmonyUITests/CliniqueHarmonyUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  CliniqueHarmonyUITestsLaunchTests.swift
+//  CliniqueHarmonyUITests
+//
+//  Created by Ana Bogdan on 09.11.2025.
+//
+
+import XCTest
+
+final class CliniqueHarmonyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
